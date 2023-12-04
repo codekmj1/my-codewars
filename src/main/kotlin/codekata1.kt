b4a311d2c2d58d47b3367a73aacaed30ae83676a
kmj1000 <rlaalswnoo@naver.com>
2023-12-04 21:20:05 +0900

    Add my first code kata

---
+++ src/main/kotlin/codekata1.kt
@@ -0,0 +1,16 @@
+// 문제1. 정수 num1과 num2가 주어질 때,
+// num1에서 num2를 뺀 값을
+// return하도록 soltuion 함수를 완성해주세요.
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+
+        var answer: Int = 0
+        // 변수 answer 에다가 num1에서 num2를 뺀 식을 대입 한다
+        answer = num1 - num2
+        return answer
+
+
+    }
+}
+
