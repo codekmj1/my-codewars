abb6fff673a92326bce8fa251d6c5dacc4df00c0
codekmj1 <codekmj@gmail.com>
2023-12-05 15:56:27 +0900

    Add my second codekata

---
+++ src/main/kotlin/codekata2.kt
@@ -0,0 +1,12 @@
+// 문제 2. 정수 num1, num2가 매개변수 주어집니다.
+// num1과 num2를 곱한 값을 return 하도록
+// solution 함수를 완성해주세요.
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        // answer 변수에 num1과 num2 곱하는 식을 대입
+        answer = num1 * num2
+        return answer
+    }
+}
