4eec77db1d6ebe3eb7ddf72aa8d96f3870fb93d6
codekmj1 <codekmj@gmail.com>
2023-12-07 22:57:28 +0900

    Add my 4th codekata

--- src/main/kotlin/codekata3.kt
+++ src/main/kotlin/codekata3.kt
@@ -2,7 +2,7 @@
 // num1을 num2로 나눈 몫을 return 하도록
 // solution 함수를 완성해주세요.
 
-class Solution {
+class Solution3 {
     fun solution(num1: Int, num2: Int): Int {
         var answer: Int = 0
         // answer 변수에 num1을 num2로 나누는 식을 대입
