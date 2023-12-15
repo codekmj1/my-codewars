e7c17b012ab3850e04575584b0c6d58be3b2076d
codekmj1 <codekmj@gmail.com>
2023-12-15 23:44:19 +0900

    Add my 10th codekata

---
+++ src/main/kotlin/codekata10.kt
@@ -0,0 +1,15 @@
+// 문제 10. 정수 배열 numbers가 매개변수로 주어집니다.
+// numbers의 원소의 평균값을 return하도록 solution
+// 함수를 완성해주세요.
+
+class Solution {
+    fun solution(numbers: IntArray): Double {
+        var answer: Double = 0.0
+        // 주어진 배열 numbers의 모든 원소를 더한다
+        for (number in numbers) {
+            answer += number
+        }
+        // 배열 크기인 numbers.size로 나눠서 평균을 구한다
+        return answer / numbers.size
+    }
+}
